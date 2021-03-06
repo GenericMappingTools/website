f3a03b163b15ffafd4933c07fdf44c0a1d44eb72
Meghan Jones <meghanj@hawaii.edu>
2021-03-06 09:22:21 -1000

    Update lithospheric-age cpt

--- _static/figures/lithospheric-age.sh
+++ _static/figures/lithospheric-age.sh
@@ -3,10 +3,11 @@ gmt begin
     gmt figure lithospheric-age PNG A,E100,Qt4,Qg2
 	# Get gradients of the relief from N45E
 	gmt grdgradient @earth_relief_20m -Nt1.25 -A45 -Gintens.nc
-    gmt basemap -JR220/20c -R@age.3.20.nc -Bf
+    gmt basemap -JR220/20c -R@earth_age_20m -Bf
+	gmt makecpt -Cinferno -T0/280/10 -Ic
 	# Plot age grid first using age cpt
-	gmt grdimage @age.3.20.nc -Iintens.nc -C@crustal_age.cpt
-    gmt colorbar -DJBC+w10c+o0/0.5c -C@crustal_age.cpt -B50 -Bx+l"Lithospheric Age [Ma]"
+	gmt grdimage @earth_age_20m -Iintens.nc
+    gmt colorbar -DJBC+w10c+o0/0.5c -B50 -Bx+l"Lithospheric Age [Ma]"
 	# Extract a topography CPT
 	gmt makecpt -Cdem2 -T0/6000
 	# Clip to expose land areas only
